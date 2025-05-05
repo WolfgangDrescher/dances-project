f098693d3b3ea978a54969aab9ed02908f914473
Wolfgang Drescher <drescher.wolfgang+git@gmail.com>
2025-05-05 12:24:08 +0200

    Add basic setup

---
+++ components/TheNavigation.vue
@@ -0,0 +1,19 @@
+<script setup>
+const { t } = useI18n();
+const localePath = useLocalePath();
+
+const items = [
+    {
+        label: t('home'),
+        to: localePath({name: 'index'}),
+    },
+    {
+        label: t('pieces'),
+        to: localePath({name: 'piece'}),
+    },
+];
+</script>
+
+<template>
+    <UNavigationMenu :items="items" class="border-b border-gray-200 dark:border-gray-800 px-2" />
+</template>
---
+++ nuxt.config.ts
@@ -0,0 +1,43 @@
+import { fileURLToPath } from 'node:url';
+
+// https://nuxt.com/docs/api/configuration/nuxt-config
+export default defineNuxtConfig({
+    compatibilityDate: '2024-11-01',
+    devtools: { enabled: false },
+    modules: [
+        '@nuxt/ui',
+        '@nuxt/fonts',
+        '@nuxtjs/i18n',
+        '@pinia/nuxt',
+        '@nuxt/content'
+    ],
+    css: ['~/assets/main.css'],
+    i18n: {
+        strategy: 'prefix_except_default',
+        locales: [
+            { code: 'de', language: 'de-DE', file: 'de.yaml', dir: 'ltr' },
+        ],
+        defaultLocale: 'de',
+        langDir: 'locales/',
+    },
+    vite: {
+        worker: {
+            format: 'es',
+        },
+        optimizeDeps: {
+            exclude: ['verovio'],
+        },
+    },
+    nitro: {
+        publicAssets: [
+            {
+                baseURL: 'kern/schubert-dances',
+                dir: fileURLToPath(new URL('./schubert-dances/kern', import.meta.url)),
+                maxAge: 3600,
+            },
+        ],
+    },
+    colorMode: {
+        preference: 'light',
+    },
+});
