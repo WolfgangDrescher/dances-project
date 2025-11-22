0818f24238d304813b0dd569269514766858e16d
percussionfloh <florian.moldaschl@web.de>
2025-11-22 16:42:15 +0100

    schau mal rein, bin nicht weiter gekommen

--- app/components/PieceForm.vue
+++ app/components/PieceForm.vue
@@ -1,12 +1,48 @@
 <script setup>
 const props = defineProps({
-    form: Array,
+    form: {
+        type: Array,
+        required: true,
+    }
 });
+
+const form = props.form || [];
+
+const formsGroupedByName = computed(() => {
+    return Object.entries(form.reduce((groups, item) => {
+		const name = props.showKeys ? item.key : item.deg;
+		if (!groups[name]) {
+			groups[name] = [];
+		}
+		groups[name].push(item);
+		return groups;
+	}, {}));
+});
+
+function getWidth(startBeat, endBeat) {
+    return (endBeat - startBeat) / maxBeat * 100;
+}
+function getLeft(startBeat) {
+    return startBeat / maxBeat * 100;
+}
+
 </script>
 
 <template>
     <div class="flex gap-4">
+        <div v-for="segment in group[1]" class="absolute h-full" :style="{
+                width: `${getWidth(segment.startBeat, segment.endBeat)}%`,
+                left: `${getLeft(segment.startBeat)}%`,
+            }">
+                <UTooltip :text="segment.key">
+                    <div class="w-full h-full bg-gray-200 rounded px-2 flex items-center hover:bg-primary-400 hover:shadow">
+                        {{ group[0] }}
+                    </div>
+                </UTooltip>
+            </div>
+            
         <div v-for="part in form" class="p-2 bg-gray-200 rounded">
+           
             {{ part.name }}
         </div>
     </div>
---
+++ app/pages/form.vue
@@ -0,0 +1,73 @@
+<script setup>
+const { data } = await useAsyncData('pieces', () => queryCollection('pieces').all())
+const { data: formData } = await useAsyncData(`form`, () => queryCollection('data').path('/data/form').first(), {deep: false });
+const localePath = useLocalePath();
+
+const { t } = useI18n();
+
+const tabItems = [
+    {
+        slot: 'minimap',
+        label: t('minimap'),
+        icon: 'i-heroicons-map',
+    }];
+
+const options = reactive({
+    showKeys: false,
+});
+
+const openScore = ref(null);
+function toggleScore(value) {
+    openScore.value = openScore.value === value ? null : value;
+}
+</script>
+
+
+<template>
+    <UContainer>
+        <Heading>{{ $t('Form') }}</Heading>
+
+        <UTabs :items="tabItems">
+
+            <template #minimap>
+                <div class="my-4 flex grow-0 flex-wrap gap-6 md:order-1">
+                    <UCheckbox v-model="options.showKeys" :label="$t('showKeys')" />
+                </div>
+                <div class="grid grid-cols-1 gap-4">
+                    <UCard v-for="piece in data">
+                        <template #header>
+                            <NuxtLink :to="localePath({ name: 'piece-id', params: { id: piece.slug } })">
+                                <div class="flex">
+                                    <div>
+                                        {{ `${piece.composer}: ${piece.largerWorkTitle} №${piece.nr}` }}
+                                    </div>
+                                    <div class="ml-auto">
+                                        {{ `Op. ${piece.op} № ${piece.nr}` }}
+                                    </div>
+                                </div>
+                            </NuxtLink>
+                        </template>
+                        <div class="grid grid-cols-1 gap-4">
+                            <div>
+                                <div class="flex gap-2 justify-end items-center mb-2">
+                                    <div>
+                                        <MidiPlayer :url="piece.localRawFile" class="text-2xl"/>
+                                    </div>
+                                    <UButton @click="toggleScore(piece.slug)" variant="soft">
+                                        {{ openScore === piece.slug ? $t('hideScore') : $t('showScore') }}
+                                    </UButton>
+                                </div>
+                                <div>
+                                    <VerovioCanvas v-if="openScore === piece.slug" view-mode="horizontal" :url="piece.localRawFile" :scale="35" :page-margin="20" />
+                                </div>
+                            </div>
+                            <PieceForm v-if="formData?.[piece.slug]" :form="formData[piece.slug]"  :show-keys="options.showKeys"/>
+
+                        </div>
+                    </UCard>
+                </div>
+            </template>
+        </UTabs>
+    </UContainer>
+</template>
+
